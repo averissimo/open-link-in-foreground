3417427f8e04b9239053edbe10381016f4ae1a66
André Veríssimo <afsverissimo@gmail.com>
2017-09-06 16:14:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Open Link in Foreground Firefox Extension
+
+Opens selected link in a foreground tab
+
+That's just it.
+
+## License
+
+[GNU General Public License Version 3](http://www.gnu.org/licenses/gpl.html)
---
+++ src/background.js
@@ -0,0 +1,33 @@
+
+
+//
+// Open Selected Links web extension
+//
+// URL/Link helper extension, primarily for dealing with mass links.
+// The extension adds context menus to handle links in various ways,
+// especially bulk actions on multiple selected links
+//
+
+//
+// Open selected links
+//
+// a context menu item that opens selected HTML links in background tabs.
+// Since a background script can't see page content we have to ask
+// the tab that was clicked to return it to us.
+//
+chrome.contextMenus.create( {
+  id: "open-link-foreground",
+  title: browser.i18n.getMessage("openLinkForeground"),
+  contexts: [ "link" ],
+  onclick: openLinkForeground
+} );
+
+function openLinkForeground( info, tab ) {
+  // prior to bug 1250631 tab was part of the info object
+  if ( !tab ) { tab = info.tab; }
+  console.log('info', info);
+  chrome.tabs.create({
+    url: info.linkUrl,
+    active: true
+  })
+}
